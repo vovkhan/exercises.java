6bf4e684caf9720f2551b765b59a0d645c0352da
vovkhan <vivaldera@gmail.com>
2025-10-23 12:54:35 -0300

    progress in Lista1

--- POO.2025.2/Lista1/src/Questao10/Problema.java
+++ POO.2025.2/Lista1/src/Questao10/Problema.java
@@ -1,4 +1,7 @@
 package Questao10;
+
+import java.util.Scanner;
+
 /*
 Faça um programa para um caixa eletrônico. Dado o valor do saque, o programa deve informar quantas notas de
 cada valor serão fornecidas. As notas disponíveis serão as de 1, 5, 10, 50 e 100 reais. O valor mínimo de saque é
@@ -10,4 +13,31 @@ nota de 1;
 nota de 5 e quatro notas de 1.
 */
 public class Problema {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] notas = {100, 50, 10, 5, 1};
+
+        int valorSaque;
+        while(true){
+            System.out.print("Insert the value to be exchanged: ");
+            valorSaque = (int) (input.nextDouble());
+            if (valorSaque >= 10 && valorSaque <= 600) {
+                break;
+            }
+        }
+
+        int[] qtdNotas = new int[notas.length];
+        int resto = valorSaque;
+        for (int i = 0; i < notas.length; i++) {
+            if (resto >= notas[i]) {
+                qtdNotas[i] = resto/notas[i];
+                resto -= qtdNotas[i]*notas[i];
+            }
+        }
+
+        System.out.printf("O valor %d resulta na seguinte combinação de notas sortidas:%n", valorSaque);
+        for (int i = 0; i < notas.length; i++) {
+            System.out.printf("%d notas de %d.%n", qtdNotas[i],  notas[i]);
+        }
+    }
 }
--- POO.2025.2/Lista1/src/Questao11/Problema.java
+++ POO.2025.2/Lista1/src/Questao11/Problema.java
@@ -1,7 +1,18 @@
 package Questao11;
 // (Deitel) Escreva um programa que, dado um número binário (isto é, um inteiro que contém somente 0s e 1s),
-// imprime seu equivalente decimal. Dica: Utilize os operadores de resto e divisão para pegar os dígitos do número
+// imprime o seu equivalente decimal. Dica: Utilize os operadores de resto e divisão para pegar os dígitos do número
 // binário um de cada vez da direita para a esquerda.
 
+import java.util.Scanner;
+
 public class Problema {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int binario;
+        System.out.print("Insert the binary value: ");
+        binario = (int) input.nextDouble();
+
+        int resto = binario;
+    }
 }
